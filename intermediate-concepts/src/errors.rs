37c88a2977da4576d8a5e544c2bf68213505816b
Joe Caulfield <joecaulfield29@yahoo.com>
2022-02-27 17:52:41 -0500

    recorded version

--- intermediate-concepts/src/errors.rs
+++ intermediate-concepts/src/errors.rs
@@ -1,5 +1,6 @@
 
 use std::process::Command;
+use std::io::ErrorKind;
 
 // Linux only
 fn error_handling_example_1(dir: &str) {
@@ -34,9 +35,32 @@ fn error_handling_example_2(dir: &str) {
     println!("\n\n");
 }
 
+// Linux only
+fn error_handling_example_3(dir: &str) {
+
+    println!("\n\n");
+
+    let mut list_cmd = Command::new("ls");
+
+    let x = match list_cmd.current_dir(dir).status() {
+        Ok(cmd) => Some(cmd),
+        Err(e) => match e.kind() {
+            ErrorKind::NotFound => {
+                println!("Directory not found!");
+                None
+            },
+            _ => panic!("An unexpected error has occurred!")
+        },
+    };
+
+    println!("\n\n");
+}
+
 fn main() {
     error_handling_example_1("src");
     error_handling_example_1("lib");
     error_handling_example_2("src");
     error_handling_example_2("lib");
+    error_handling_example_3("src");
+    error_handling_example_3("lib");
 }
