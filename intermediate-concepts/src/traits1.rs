f2e331053161cd6a5a192f89fde1574159290e41
jpcaulfi <joecaulfield29@yahoo.com>
2022-03-01 09:05:49 -0500

    recorded version

--- intermediate-concepts/src/traits1.rs
+++ intermediate-concepts/src/traits1.rs
@@ -1,38 +1,45 @@
 
 pub trait Vehicle {
+
     fn forward() -> String;
     fn backward() -> String;
-}
 
-pub struct Truck {
-    color: String,
+    fn turn_ignition() -> String {
+        String::from("vroom vroom")
+    }
 }
 
 pub struct Car {
     color: String,
 }
 
-impl Vehicle for Truck {
-    fn forward() -> String {
-        String::from("Driving my truck forward!")
-    }
-    fn backward() -> String {
-        String::from("Backing my truck up...")
-    }
-}
-
 impl Vehicle for Car {
     fn forward() -> String {
-        String::from("Driving my car forward!")
+        String::from("Driving my car forward")
     }
     fn backward() -> String {
         String::from("Backing my car up...")
     }
 }
 
+pub struct Truck {
+    color: String,
+}
+
+impl Vehicle for Truck {
+    fn forward() -> String {
+        String::from("Driving my truck forward")
+    }
+    fn backward() -> String {
+        String::from("Backing my truck up...")
+    }
+}
+
 fn main() {
+    println!("{}", Car::turn_ignition());
     println!("{}", Car::forward());
     println!("{}", Car::backward());
+    println!("{}", Truck::turn_ignition());
     println!("{}", Truck::forward());
     println!("{}", Truck::backward());
 }
--- intermediate-concepts/src/traits2.rs
+++ intermediate-concepts/src/traits2.rs
@@ -1,41 +1,51 @@
 
 pub trait Vehicle {
+
     fn forward(&self) -> String;
     fn backward(&self) -> String;
-}
 
-pub struct Truck {
-    color: String,
+    fn turn_ignition() -> String {
+        String::from("vroom vroom")
+    }
 }
 
 pub struct Car {
     color: String,
 }
 
-impl Vehicle for Truck {
-    fn forward(&self) -> String {
-        String::from(format!("Driving my {} truck forward!", self.color))
-    }
-    fn backward(&self) -> String {
-        String::from(format!("Backing my {} truck up...", self.color))
-    }
-}
-
 impl Vehicle for Car {
     fn forward(&self) -> String {
-        String::from(format!("Driving my {} car forward!", self.color))
+        String::from(format!("Driving my {} car forward", self.color))
     }
     fn backward(&self) -> String {
         String::from(format!("Backing my {} car up...", self.color))
     }
 }
 
+pub struct Truck {
+    color: String,
+}
+
+impl Vehicle for Truck {
+    fn forward(&self) -> String {
+        String::from(format!("Driving my {} truck forward", self.color))
+    }
+    fn backward(&self) -> String {
+        String::from(format!("Backing my {} truck up...", self.color))
+    }
+}
 
 fn main() {
-    let my_truck = Truck { color: String::from("green") };
-    let my_car = Car { color: String::from("red") };
-    println!("{}", my_car.forward());
-    println!("{}", my_car.backward());
-    println!("{}", my_truck.forward());
-    println!("{}", my_truck.backward());
+    
+    println!("{}", Car::turn_ignition());
+    
+    let car = Car{color: String::from("green")};
+    println!("{}", car.forward());
+    println!("{}", car.backward());
+    
+    println!("{}", Truck::turn_ignition());
+    
+    let truck = Truck{color: String::from("blue")};
+    println!("{}", truck.forward());
+    println!("{}", truck.backward());
 }
